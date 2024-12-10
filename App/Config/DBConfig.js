82e71d5d3d999abad172e59d89f77d5226c2e559
kanimozhis-4 <kanimozhi808088@gmail.com>
2024-12-10 10:49:46 +0530

    added pragma foreign key in sqlite

--- App/Config/DBConfig.js
+++ App/Config/DBConfig.js
@@ -4,8 +4,13 @@ const db = new sqlite3.Database('./connect.db', (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else { 
-      
+
         console.log('Connected to the SQLite database.');
+        db.run("PRAGMA foreign_keys = ON;", (err) => {
+          if (err) {
+              console.error("Error enabling foreign keys:", err.message);
+          } 
+      });
     }
 });
 
